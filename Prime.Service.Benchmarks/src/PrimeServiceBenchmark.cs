749f71f4b93ba31b8bd97b3a949d17aa5e3ecd21
Sebastian Teumert <sebastian.teumert@gmx.de>
2020-10-29 23:54:57 +0100

    Add benchmark action

--- Prime.Service.Benchmarks/src/PrimeServiceBenchmark.cs
+++ Prime.Service.Benchmarks/src/PrimeServiceBenchmark.cs
@@ -12,14 +12,14 @@ namespace Prime.Services.Benchmarks
         private readonly PrimeService service = new PrimeCalculator();
         private readonly Consumer consumer = new Consumer();
 
-        [Params(100, 200, 500, 1000)]
-        public int Count { get; set; }
+        // [Params(100, 200, 500, 1000)]
+        // public int Count { get; set; }
 
-        [Params(0, 1000, 2000, 5000, 10000)]
-        public int Offset { get; set; }
+        // [Params(0, 1000, 2000, 5000, 10000)]
+        // public int Offset { get; set; }
 
 
         [Benchmark]
-        public void FirstN () => service.GetPrimes(Offset).Take(Count).Consume(consumer);
+        public void FirstN () => service.GetPrimes(N*10).Take(N).Consume(consumer);
     }
 }
