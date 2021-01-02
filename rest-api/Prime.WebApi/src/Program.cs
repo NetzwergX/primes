39f114dc144c62f15d43d41987f60ba2a0ccc95a
Sebastian Teumert <sebastian.teumert@gmx.de>
2021-01-02 11:44:22 +0100

    Fix docker image

--- rest-api/Prime.WebApi/src/Program.cs
+++ rest-api/Prime.WebApi/src/Program.cs
@@ -22,6 +22,7 @@ namespace Prime.WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    //webBuilder.UseUrls("http://0.0.0.0:5000/");
                 });
     }
 }
